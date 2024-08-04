fcfab6d8088bea5eaae0a6aaae908895872d7efd
Rocketake <onazik08072005@gmail.com>
2024-08-04 23:02:56 +0300

    complete

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,29 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-	const totalOrderPrice = quantity * pricePerDroid;
-	if (totalOrderPrice > customerCredits) {
-		return "Insufficient funds!"
+function isEnoughCapacity(products, containerSize) {
+	const values = Object.values(products);
+	
+	let total = 0;
+
+	for (const value of values) {
+		total += value;
 	}
-	else {
-		return `You ordered ${quantity} droids worth ${totalOrderPrice} credits!`
-	}
+
+	return total <= containerSize;
 }
+
 console.log("Task-1")
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
